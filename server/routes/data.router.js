83c83fbe86dc192f6a43f4181fe0b075ce980e70
Daniel Shugarts <daniel.shugarts@gmail.com>
2018-03-13 14:59:32 -0500

    data router created

---
+++ server/routes/data.router.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const pool = require('../modules/pool.js');
+const router = express.Router();
+
+
+
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const sessionConfig = require('./modules/session-middleware');
 
 // Route includes
 const userRouter = require('./routes/user.router');
+const dataRouter = require('./routes/data.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -21,6 +22,7 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
+app.use('/data', dataRouter);
 
 // Serve static files
 app.use(express.static('server/public'));
