46ca4da241b6c835ba7b125aa66c1483204cbb73
Daniel Shugarts <daniel.shugarts@gmail.com>
2018-03-13 10:23:45 -0500

    database.sql added

---
+++ database.sql
@@ -0,0 +1,34 @@
+CREATE TABLE users (
+  user_id serial primary key,
+  username varchar(80) not null UNIQUE,
+  password varchar(240) not null
+);
+
+CREATE TABLE data (
+	entry_id serial primary key,
+	user_id INT REFERENCES users ON DELETE CASCADE,
+	age_value INT,
+	family_history_value BOOLEAN,
+	physical_activity_value INT,
+	systolic_value INT,
+	diastolic_value INT,
+	nicotine_value BOOLEAN,
+	glu_value INT,
+	hdl_value INT,
+	ldl_value INT,
+	waist_value DECIMAL(3,1),
+	sleep_value BOOLEAN,
+	height_value DECIMAL(4,2),
+	weight_value DECIMAL(4,1),
+	age_score INT,
+	activity_score INT,
+	history_score INT,
+	bp_score INT,
+	nicotine_score INT,
+	glu_score INT,
+	hdl_score INT,
+	ldl_score INT,
+	waist_score INT,
+	sleep_score INT,
+	cvd_score INT
+);
