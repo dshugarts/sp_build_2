f748b888db0c889037904ca32e1fccaa75314f15
Daniel Shugarts <daniel.shugarts@gmail.com>
2018-03-20 13:35:40 -0500

    all charts views controllers and links created but no data

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -49,6 +49,38 @@ myApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $l
       templateUrl: '/views/charts/glucose.html',
       controller: 'GlucoseController as vm',
     })
+    .when('/hdl', {
+      templateUrl: '/views/charts/hdl.html',
+      controller: 'HDLController as vm',
+    })
+    .when('/ldl', {
+      templateUrl: '/views/charts/ldl.html',
+      controller: 'LDLController as vm',
+    })
+    .when('/trg', {
+      templateUrl: '/views/charts/trg.html',
+      controller: 'TRGController as vm',
+    })
+    .when('/hearthealth', {
+      templateUrl: '/views/charts/hearthealth.html',
+      controller: 'HeartHealthController as vm',
+    })
+    .when('/waist', {
+      templateUrl: '/views/charts/waist.html',
+      controller: 'WaistController as vm',
+    })
+    .when('/sleep', {
+      templateUrl: '/views/charts/sleep.html',
+      controller: 'SleepController as vm',
+    })
+    .when('/weight', {
+      templateUrl: '/views/charts/weight.html',
+      controller: 'WeightController as vm',
+    })
+    .when('/bp', {
+      templateUrl: '/views/charts/bp.html',
+      controller: 'BPController as vm',
+    })
     .otherwise({
       template: '<h1>404</h1>'
     });
---
+++ server/public/scripts/controllers/ldl.controller.js
@@ -0,0 +1,45 @@
+myApp.controller('LDLController', ['$http', '$location', 'UserService', 'PostDataService', function($http, $location, UserService, PostDataService) {
+    console.log('LDLController created');
+    var self = this;
+    self.userService = UserService;
+    self.postdataService = PostDataService;
+    self.userObject = UserService.userObject;
+    self.id = UserService.userObject.id;
+    self.getDates = PostDataService.getDates;
+    self.datesArray = PostDataService.datesArray;
+    self.gluScore = PostDataService.gluScore;
+  
+
+
+    console.log('GCAA', self.gluScore);
+
+
+    new Chart(document.getElementById("line-chart"), {
+        type: 'line',
+        data: {
+          labels: [1, 2, 3, 4, 5],
+          datasets: [{ 
+              data: [self.gluScore[0], self.gluScore[1], self.gluScore[2], self.gluScore[3], self.gluScore[4]],
+              label: "Glucose",
+              borderColor: "crimson",
+              fill: false
+            }]
+        },
+        options: {
+          title: {
+            display: true,
+            text: 'My Glucose Readings Over Time',
+            position: 'bottom'
+          },
+          legend: {
+              position: 'bottom'
+          },
+          layout: {
+            padding: {
+              top: 10
+            }
+          }
+        }
+      });
+
+}]);
